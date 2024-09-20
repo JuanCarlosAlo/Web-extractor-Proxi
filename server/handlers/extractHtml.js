8f1c7a321bc70abb21b2ad5c68a0531c69ff7354
JC <jc.alonsome@gmail.com>
2024-09-20 12:08:04 +0200

    token browserless

--- server/handlers/extractHtml.js
+++ server/handlers/extractHtml.js
@@ -3,40 +3,51 @@ const { JSDOM } = require('jsdom');
 const beautify = require('js-beautify').html;
 
 async function extractHtml(url) {
-  // Conexión a la instancia de Browserless en Railway
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: 'wss://browserless-production-e893.up.railway.app' // URL de tu instancia de Browserless
-  });
+    let browser;
+    try {
+        // Conexión a la instancia de Browserless en Railway
+        browser = await puppeteer.connect({
+            browserWSEndpoint: 'wss://browserless-production-e893.up.railway.app?token=QspLDpEcp8lmujdfa6d80878f005b406209e9c25e2' // URL de tu instancia de Browserless
+        });
 
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-  const html = await page.content();
-  await browser.close();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
+        const html = await page.content();
 
-  // Extraer las secciones del HTML
-  const extractedHtml = extractSections(html, url);
-  return beautify(extractedHtml, { indent_size: 2, space_in_empty_paren: true });
+        // Extraer las secciones del HTML
+        const extractedHtml = extractSections(html, url);
+        return beautify(extractedHtml, { indent_size: 2, space_in_empty_paren: true });
+
+    } catch (error) {
+        console.error('Error during HTML extraction:', error);
+        throw new Error('Error during HTML extraction');
+    } finally {
+        // Cerrar el navegador solo si se abrió
+        if (browser) {
+            await browser.close();
+        }
+    }
 }
 
 function extractSections(html, baseUrl) {
-  const dom = new JSDOM(html);
-  const { document } = dom.window;
+    const dom = new JSDOM(html);
+    const { document } = dom.window;
 
-  let htmlContent = '';
+    let htmlContent = '';
 
-  // Obtener todas las secciones <section> del documento
-  const allSections = document.querySelectorAll('section');
-  allSections.forEach(section => {
-    htmlContent += section.outerHTML;
-  });
+    // Obtener todas las secciones <section> del documento
+    const allSections = document.querySelectorAll('section');
+    allSections.forEach(section => {
+        htmlContent += section.outerHTML;
+    });
 
-  const baseDomain = new URL(baseUrl).origin;
-  return htmlContent.replace(new RegExp(baseDomain, 'g'), '/wp-content')
-    .replace(/(href|src)="\/([^"]+)"/g, (match, attr, url) => `${attr}="${baseDomain}/${url}"`)
-    .replace(/url\(&quot;/g, 'url("')
-    .replace(/&quot;\)/g, '")')
-    .replace(/url\(&#34;/g, 'url("')
-    .replace(/&#34;\)/g, '")');
+    const baseDomain = new URL(baseUrl).origin;
+    return htmlContent.replace(new RegExp(baseDomain, 'g'), '/wp-content')
+        .replace(/(href|src)="\/([^"]+)"/g, (match, attr, url) => `${attr}="${baseDomain}/${url}"`)
+        .replace(/url\(&quot;/g, 'url("')
+        .replace(/&quot;\)/g, '")')
+        .replace(/url\(&#34;/g, 'url("')
+        .replace(/&#34;\)/g, '")');
 }
 
 module.exports = { extractHtml };
